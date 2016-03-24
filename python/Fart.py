589e75d7874d0a55ce1b99a4e82af883c4bbc3eb
FreddyKilo <freddy.kilo@gmail.com>
2016-03-23 20:36:33 -0700

    add Fart class

---
+++ python/Fart.py
@@ -0,0 +1,14 @@
+from random import randint
+import os
+
+class Fart():
+
+    def __init__(self):
+        pass#gas
+
+    def randomShat():
+        specify(randint(0,15))
+
+    def specifyStank(num):
+        if num >= 0 and num < 16:
+            os.system("omxplayer ~/Mr_Roboto/audio/Fart_" + num)
