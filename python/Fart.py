d5817f5d7fff9122965e9edf99925e54dea28911
fred.kudelka <fred.kudelka@etw.com>
2016-03-23 21:43:58 -0700

    fix Fart.py

--- python/Fart.py
+++ python/Fart.py
@@ -1,14 +1,11 @@
 from random import randint
 import os
 
-class Fart():
+class Fart:
 
-    def __init__(self):
-        pass#gas
-
-    def randomShat():
-        specify(randint(0,15))
-
-    def specifyStank(num):
+    def specifyStank(self, num):
         if num >= 0 and num < 16:
-            os.system("omxplayer ~/Mr_Roboto/audio/Fart_" + num)
+            os.system("afplay ~/Mr_Roboto/audio/Fart_{}.mp3".format(num))
+
+    def randomShat(self):
+        os.system("afplay ~/Mr_Roboto/audio/Fart_{}.mp3".format(randint(0,15)))
