2eac7979c1a394301ad4f559534cbcfdc6b25c1d
FreddyKilo <freddy.kilo@gmail.com>
2016-03-23 21:45:14 -0700

    huh

--- python/Fart.py
+++ python/Fart.py
@@ -1,14 +1,14 @@
 from random import randint
 import os
 
-class Fart():
+class Fart:
 
     def __init__(self):
         pass#gas
 
-    def randomShat():
-        specify(randint(0,15))
+    def randomShat(self):
+        specifyStank(randint(0,15))
 
-    def specifyStank(num):
+    def specifyStank(self, num):
         if num >= 0 and num < 16:
             os.system("omxplayer ~/Mr_Roboto/audio/Fart_" + num)
